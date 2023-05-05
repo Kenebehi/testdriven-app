5b085d46951ef72bc1b6597c4f95ac2427f66b81
kenneth <kenneth.odumah@gmail.com>
2023-05-05 10:22:53 +0200

    testing corner cases

--- test.sh
+++ test.sh
@@ -9,15 +9,59 @@
 
 
 
- PATHS="./services/hello.py   ./services/test.py  ./service/testcopy.py services/workers/services/new.py"
- FILES="$(echo $PATHS | tr ' ' '\n' | grep "services/workers/.*\.py" | awk -F/ '{print $NF}' | tr '\n' ' ')"
-echo $FILES
+#PATHS="./services/hello.py   ./services/test.py  ./service/testcopy.py services/workers/services/new.py"
+#FILES="$(echo $PATHS | tr ' ' '\n' | grep "services/workers/.*\.py" | awk -F/ '{print $NF}' | tr '\n' ' ')"
+#echo $FILES
+#
+#INCLUDES=""
+#for path in $FILES; do
+#    INCLUDES="$INCLUDES --include $path"
+#done
+#echo $INCLUDES
 
-INCLUDES=""
-for path in $FILES; do
-    INCLUDES="$INCLUDES --include $path"
+
+#!/bin/bash
+
+## set the path to the input path
+#full_path="airflow/utils/dag_utils/spark_etl.py"
+#filename="$(basename "$full_path")"  # extracts "hello.py"
+#dirname="$(dirname "$full_path")"   # extracts "foo/bar/baz"
+#parentdir="$(basename "$dirname")"  # extracts "baz"
+#new_path="${parentdir}/${filename}" # creates "baz/hello.py"
+#echo "$new_path"  # prints "baz/hello.py"
+
+#include=""
+#for file in $full_path; do
+#  include="$include --include $(basename "$(dirname "$file")")/$(basename "$file")"
+#done
+#echo $include
+
+
+#path="airflow/settings/subfolder/settings.yml   airflow/settings/settings.yml"
+#include=""
+#for file in $path; do
+#  include="$include --include $(expr $file : '[^/]*/\(.*\)')"
+#done
+#echo $include
+
+
+#include=""
+#for file in "airflow/settings/globals.yml" "airflow/settings/subfolder/settings.yml"; do
+#  include="$include --include $( $file | sed 's/^[^\/]*\///')"
+#done
+#echo $include
+
+include=""
+for file in "airflow/settings/globals.yml" "airflow/settings/subfolder/settings.yml"; do
+  include="$include --include ${file#*/}"
 done
-echo $INCLUDES
+echo "$include"
+
+
+
+
+
+
 
 
 
